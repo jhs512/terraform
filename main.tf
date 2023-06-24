bb0af089ea8e63fa36cca1a9968601388eb8112b
jhs512 <jangka512@gmail.com>
2023-06-24 16:31:51 +0900

    서브넷

--- main.tf
+++ main.tf
@@ -29,3 +29,13 @@ resource "aws_vpc" "vpc" {
     Name = "${var.prefix}-vpc"
   }
 }
+
+# Add a subnet
+resource "aws_subnet" "subnet" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "${var.prefix}-subnet"
+  }
+}
