95f3c3848c4e60faf0dd587d4b0ee28b7e3247a4
jhs512 <jangka512@gmail.com>
2023-06-24 16:29:21 +0900

    vpc

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
 terraform {
+  cloud {
+    organization = "lldj-01"
+    hostname     = "app.terraform.io"
+
+    workspaces {
+      name = "workspace-01"
+    }
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -13,6 +22,10 @@ provider "aws" {
 }
 
 # Create a VPC
-resource "aws_vpc" "example" {
+resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "${var.prefix}-vpc"
+  }
 }
